ae98d8fcbd085c3473f0406d640b0d1636c338bc
Mohamed Alaa <mohamedalaasalem1@gmail.com>
2023-09-24 01:15:15 +0200

    End of the course

--- BallLauncher_Coursee/Assets/BallController.cs
+++ BallLauncher_Coursee/Assets/BallController.cs
@@ -7,10 +7,14 @@ using UnityEngine.InputSystem.Controls;
 public class BallController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D ballRigidBody;
+    [SerializeField] SpringJoint2D ballSpringJoint;
+    [SerializeField] float deAttachTime;
+    [SerializeField] float ballMass;
+    bool isDragging;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRigidBody.mass = ballMass;
     }
 
     // Update is called once per frame
@@ -18,6 +22,7 @@ public class BallController : MonoBehaviour
     {
         if(Touchscreen.current.primaryTouch.press.isPressed)
         {
+            isDragging = true;
             ballRigidBody.isKinematic = true;
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(touchPosition);
@@ -25,7 +30,23 @@ public class BallController : MonoBehaviour
         }
         else
         {
-            ballRigidBody.isKinematic = false;
+            if(isDragging == true)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+            
         }
+    }
+
+    public void LaunchBall()
+    {
+        ballRigidBody.isKinematic = false;
+        Invoke("DeAttachBall", deAttachTime);
+    }
+
+    public void DeAttachBall()
+    {
+        ballSpringJoint.enabled = false;
     }
 }
